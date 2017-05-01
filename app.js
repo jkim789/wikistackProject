e7673217560668953ab5f9e3a2a82da44f03648d
David R Lowry Lowry <ross@Davids-MacBook-Pro.local>
2017-05-01 11:38:25 -0400

    up to querying models part

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const morgan = require('morgan')
 const nunjucks = require('nunjucks')
 const path = require('path')
 const app = express()
+var models = require('./models');
+var PORT = 3000
 
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(morgan('dev'))
@@ -11,11 +13,18 @@ nunjucks.configure('views', {noCache: true})  //using nunjucks to render.
 app.set('view engine','html')
 app.engine('html',nunjucks.render)
 
-
-var PORT = 3000
-app.listen(PORT, function(){
-    console.log('Our server is on!')
+models.User.sync({})
+.then(function () {
+  return models.Page.sync({})
 })
+.then(function () {
+  app.listen(PORT, function(){
+    console.log('Our server is on!')
+  })
+})
+.catch(console.error)
+
+
 
 app.get('/', function(req,res,next){
     res.render('index')
---
+++ models/index.js
@@ -0,0 +1,55 @@
+var Sequelize = require('sequelize');
+var db = new Sequelize('postgres://localhost:5433/wikistack', {
+  logging: false
+});
+
+var Page = db.define('page', {
+  title: {
+    type: Sequelize.STRING
+    allowNull: false
+  },
+  urltitle: {
+    type: Sequelize.STRING,
+    allowNull: false
+    validate: {
+      isUrl: true
+    }
+  },
+  content: {
+    type: Sequelize.TEXT
+    allowNull: false
+  },
+  status: {
+    type: Sequelize.ENUM('open', 'closed'),
+    // defaultValue: 'open'
+    // allowNull: true
+  },
+  date: {
+    type: Sequelize.DATE,
+    defaultValue: Sequelize.NOW
+  },
+  getterMethods: {
+    route: this.urltitle + '/wiki/'
+  }
+})
+
+
+var User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true
+    }
+  }
+})
+
+module.exports = {
+  db: db,
+  Page: Page,
+  User: User
+}
